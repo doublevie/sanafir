70ad5cb7488fe2ed594ec279a374bacf782dfd30
fares ABADOU <fares.abadou@gmail.com>
2017-05-31 00:25:38 +0100

    ini

---
+++ inc/menu.php
@@ -0,0 +1,19 @@
+<div class="mainmenu">
+
+<ul>
+  <li><a href="main.php"><i class="fa fa-home"></i></a></li>
+  <li> <i class="fa fa-address-book"> </i></li>
+  <li>TEST 2 </li>
+
+</ul>
+<ul class="right">
+
+
+  <li><i class="fa fa-user-circle"></i></li>
+  <li><i class="fa fa-sign-out"></i></li>
+
+
+  <li><i class="fa fa-bar-chart"></i></li>
+  <li><a href="settings.php"><i class="fa fa-cog"></i></a></li>
+</ul>
+</div>
